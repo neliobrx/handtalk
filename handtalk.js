ef066e8908e3d1541560ccc7fa1948066d42ebe7
Nelio Alencar <62576568+neliobrx@users.noreply.github.com>
2023-11-07 07:30:03 -0300

    Update handtalk.js

--- handtalk.js
+++ handtalk.js
@@ -1,1 +1,51 @@
-document.addEventListener("DOMContentLoaded",function(){function a(){for(var a,b=["questao1","questao3","questao7","questao10"],c=0;c<b.length;c++)if(a=b[c],!document.querySelector("input[name=\""+a+"\"]:checked"))return alert("Por favor, responda a quest\xE3o: "+a),!1;return!0}function b(){var a={questao1:document.querySelector("input[name=\"questao1\"]:checked")?document.querySelector("input[name=\"questao1\"]:checked").value:null,questao2:document.querySelector("textarea[name=\"questao2\"]").value,questao3:document.querySelector("input[name=\"questao3\"]:checked")?document.querySelector("input[name=\"questao3\"]:checked").value:null,questao4:document.querySelector("textarea[name=\"questao4\"]").value,questao5:document.querySelector("textarea[name=\"questao5\"]").value,questao6:document.querySelector("textarea[name=\"questao6\"]").value,questao7:document.querySelector("textarea[name=\"questao7\"]").value,questao8:document.querySelector("textarea[name=\"questao8\"]").value,questao9:document.querySelector("textarea[name=\"questao9\"]").value,questao10:document.querySelector("textarea[name=\"questao10\"]").value};return a}function c(a){console.log("Dados do Formul\xE1rio:",a)}var d=document.getElementById("questionnaireForm");d.addEventListener("submit",function(d){if(d.preventDefault(),a()){var e=b();c(e),alert("Formul\xE1rio enviado com sucesso!")}else alert("Por favor, complete todas as perguntas obrigat\xF3rias.")})});
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('questionnaireForm');
+
+    // Validacao formulario
+    function validateForm() {
+        var requiredQuestions = ['questao1', 'questao3', 'questao7', 'questao10'];
+        for (var i = 0; i < requiredQuestions.length; i++) {
+            var question = requiredQuestions[i];
+            if (!document.querySelector('input[name="' + question + '"]:checked')) {
+                alert('Por favor, responda a questão: ' + question);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Coletar dados do formulário
+    function collectFormData() {
+        var formData = {
+            questao1: document.querySelector('input[name="questao1"]:checked') ? document.querySelector('input[name="questao1"]:checked').value : null,
+            questao2: document.querySelector('textarea[name="questao2"]').value,
+            questao3: document.querySelector('input[name="questao3"]:checked') ? document.querySelector('input[name="questao3"]:checked').value : null,
+            questao4: document.querySelector('textarea[name="questao4"]').value,
+            questao5: document.querySelector('textarea[name="questao5"]').value,
+            questao6: document.querySelector('textarea[name="questao6"]').value,
+            questao7: document.querySelector('textarea[name="questao7"]').value,
+            questao8: document.querySelector('textarea[name="questao8"]').value,
+            questao9: document.querySelector('textarea[name="questao9"]').value,
+            questao10: document.querySelector('textarea[name="questao10"]').value
+        };
+        return formData;
+    }
+
+    // Processando as respostas do formulário
+    function processFormResponses(formData) {
+        console.log('Dados do Formulário:', formData);
+    }
+
+    // Event listener para o envio do formulário
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        if (validateForm()) {
+            var formData = collectFormData();
+            processFormResponses(formData);
+            alert('Formulário enviado com sucesso!');
+        } else {
+            alert('Por favor, complete todas as perguntas obrigatórias.');
+        }
+    });
+});
